974cbd930ff94407307b877635d61c51c78a4253
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-08-30 21:25:03 -0300

    responsividade header ajustada

--- Componentes/Base/base.js
+++ Componentes/Base/base.js
@@ -3,9 +3,6 @@ function gerarHeader() {
     let header = `
         <nav class="container-fluid" style="background: #212122;">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-                </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="../Conhecimentos/conhecimentos.html" class="nav-link text-secondary">Conhecimentos</a></li>
